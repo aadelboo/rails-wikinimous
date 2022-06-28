cc9e4a5c5707b4d09e149aa389ce5a450788e975
Aadel <85813017+aadelboo@users.noreply.github.com>
2022-06-28 22:47:53 +0200

    added CRUD actions and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'faker'
+
+10.times do |article|
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph)
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
